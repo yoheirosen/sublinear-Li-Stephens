59ce35fcea1044671419e2e43313bbec8b9714bd
yoheirosen <yrosen@berkeley.edu>
2017-05-18 20:37:17 -0700

    needed nearest site below position function

--- lh_input_haplotype.cpp
+++ lh_input_haplotype.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
 #include "lh_input_haplotype.hpp"
 
 using namespace std;
+
+inputHaplotype::~inputHaplotype() {
+  
+}
 
 inputHaplotype::inputHaplotype(vector<alleleValue> query) : alleles(query) {
   augmentations = vector<size_t>(query.size(), 0);
@@ -44,7 +49,7 @@ inputHaplotype::inputHaplotype(string query, string reference_sequence,
                 reference->get_reference_allele_at_site(i)));
     if(reference->has_span_after(i)) {
       size_t counter = 0;
-      for(size_t j = pos + 1; j <= pos + reference->span_length_after(i); i++) {
+      for(size_t j = pos + 1; j <= pos + reference->span_length_after(i); j++) {
         if(query[j] != reference_sequence[j]) {
           counter++;
         }
@@ -83,8 +88,36 @@ void inputHaplotype::edit(size_t position, char new_c, char old_c, char ref) {
     if(position < reference->get_position(0)) {
       augmentations[0] += delta_aug;
     } else {
-      augmentations[reference->get_site_index(position) + 1] +=
-                delta_aug;
+      size_t site_index = find_site_below(position);
+      augmentations[site_index + 1] += delta_aug;
+    }
+  }
+}
+
+size_t inputHaplotype::find_site_below(size_t p) {
+  if(reference->is_site(p)) {
+    return reference->get_site_index(p);
+  } else {
+    size_t lower_bound = 0;
+    size_t upper_bound = reference->number_of_sites() - 1;
+    if(p > reference->get_position(upper_bound)) {
+      return upper_bound;
+    }
+    size_t mid = upper_bound/2;
+    while(true) {
+      if(reference->get_position(mid) > p) {
+        if(mid - lower_bound == 1) {
+          return lower_bound;
+        }
+        upper_bound = mid;
+        mid = (upper_bound - lower_bound)/2 + lower_bound;
+      } else {
+        if(upper_bound - mid == 1) {
+          return mid;
+        }
+        lower_bound = mid;
+        mid = (upper_bound - lower_bound)/2 + lower_bound;
+      }
     }
   }
 }
@@ -105,3 +138,13 @@ size_t inputHaplotype::get_augmentations(int j) {
   return augmentations[j + 1];
 }
 
+// void inputHaplotype::build_relative_positions() {
+//   start_index = find_start_index();
+//   end_index = find_end_index();
+//   left_tail_length = reference->get_position(start_index) - start_position;
+//   right_tail_length = end_position - reference->get_position(end_index);
+// }
+
+// size_t inputHaplotype::get_global_index(size_t i) {
+//   return i - start_index;
+// }
--- lh_input_haplotype.hpp
+++ lh_input_haplotype.hpp
@@ -12,6 +12,21 @@ private:
   linearReferenceStructure *reference = NULL;
   vector<alleleValue> alleles;
   vector<size_t> augmentations;
+  
+  // absolute positions of start and end
+  size_t start_position;
+  size_t end_position;
+  // indices w.r.t. the reference structure of the first and last sites covered
+  // by the haplotype (the haplotype may begin and end in the middle of a span)
+  size_t start_index;
+  size_t end_index;
+  // first and last span lengths
+  size_t left_tail_length;
+  size_t right_tail_length;
+  
+  // void build_relative_positions();
+  // size_t find_start_index();
+  // size_t find_end_index();
 public:
   inputHaplotype(vector<alleleValue> query, vector<size_t> augmentation_count);
   inputHaplotype(vector<alleleValue> query, vector<size_t> augmentation_count,
@@ -30,6 +45,10 @@ public:
   
   alleleValue get_allele(size_t j);
   size_t get_augmentations(int j);
+  
+  size_t find_site_below(size_t p);
+  
+  // size_t get_global_index(size_t i);
 };
 
 #endif
