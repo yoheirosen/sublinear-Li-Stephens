2a0e6628bab858a34ced23b79421075b97fd60bc
yoheirosen <yrosen@berkeley.edu>
2017-07-11 16:36:02 -0700

    fixed overwritten lh_DP_map header file; refactored compose function

--- lh_DP_map.cpp
+++ lh_DP_map.cpp
@@ -11,12 +11,10 @@ double DPUpdateMap::evaluate_at(double x) {
   return coefficient + logsum(x, constant);
 }
 
-// Composes the maps f1: x |-> A1(x + B1) and f2: x |-> A2(x + B2) to form a map
-// f': x |-> A2A1(x + B1 + B2/A1). Performs this in log-space
-DPUpdateMap DPUpdateMap::compose(DPUpdateMap outer, DPUpdateMap inner) {
+DPUpdateMap DPUpdateMap::compose(DPUpdateMap inner) {
   DPUpdateMap to_return;
-  to_return.coefficient = outer.coefficient + inner.coefficient; 
-  to_return.constant = logsum(inner.constant, 
-                              outer.constant - inner.coefficient);
+  to_return.coefficient = coefficient + inner.coefficient; 
+  to_return.constant = logsum(constant, 
+                              constant - inner.coefficient);
   return to_return;
 }
--- lh_DP_map.hpp
+++ lh_DP_map.hpp
@@ -1,22 +1,20 @@
+#ifndef LH_DP_STATE_MAP
+#define LH_DP_STATE_MAP
+
 #include "lh_math.hpp"
-#include "lh_DP_map.hpp"
 
-DPUpdateMap::DPUpdateMap() {}
+struct DPUpdateMap{
+  double coefficient;
+  double constant;
 
-DPUpdateMap::DPUpdateMap(double coefficient, double constant) : 
-          coefficient(coefficient), constant(constant) {  
-}
+  DPUpdateMap();
+  DPUpdateMap(double coefficient, double constant);
 
-double DPUpdateMap::evaluate_at(double x) {
-  return coefficient + logsum(x, constant);
-}
+  double evaluate_at(double x);
 
-// Composes the maps f1: x |-> A1(x + B1) and f2: x |-> A2(x + B2) to form a map
-// f': x |-> A2A1(x + B1 + B2/A1). Performs this in log-space
-DPUpdateMap DPUpdateMap::compose(DPUpdateMap outer, DPUpdateMap inner) {
-  DPUpdateMap to_return;
-  to_return.coefficient = outer.coefficient + inner.coefficient; 
-  to_return.constant = logsum(inner.constant, 
-                              outer.constant - inner.coefficient);
-  return to_return;
-}
+  // Composes the maps f1: x |-> A1(x + B1) and f2: x |-> A2(x + B2) to form a map
+  // f': x |-> A2A1(x + B1 + B2/A1). Performs this in log-space
+  DPUpdateMap compose(DPUpdateMap inner);
+};
+
+#endif
