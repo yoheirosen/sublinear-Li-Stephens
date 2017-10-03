b32106403df0058da466e5e1a0f65d5eae41edc8
yoheirosen <yrosen@berkeley.edu>
2017-10-03 11:37:38 -0700

    efficiency corrections and empty-rowSet size accession error corrected

--- src/row_set.cpp
+++ src/row_set.cpp
@@ -4,14 +4,24 @@ rowSet::rowSet() {
   
 }
 
-rowSet::rowSet(vector<const vector<size_t>* > row_vectors, vector<alleleValue> alleles) : included_alleles(alleles), row_vectors(row_vectors) {
-  for(size_t i = 1; i < row_vectors.size(); i++) {
-    boundaries.push_back(boundaries.back() + row_vectors[i - 1]->size());
+rowSet::rowSet(vector<const vector<size_t>* > row_vectors, 
+               vector<alleleValue> alleles) :
+                        included_alleles(alleles), 
+                        row_vectors(row_vectors) {
+  unique_alleles = row_vectors.size();
+  if(row_vectors.size() == 0) {
+    elements = 0;
+  } else {
+    for(size_t i = 1; i < unique_alleles; i++) {
+      boundaries.push_back(boundaries.back() + row_vectors[i - 1]->size());
+    }
+    elements = boundaries.back() + row_vectors.back()->size();
   }
 }
 
-size_t rowSet::operator[](size_t i) const {
-  for(size_t j = boundaries.size() - 1; j >= 0; j--) {
+const size_t& rowSet::operator[](size_t i) const {
+  // TODO: make optimized accessor for loop
+  for(size_t j = unique_alleles - 1; j >= 0; j--) {
     if(i >= boundaries[j]) {
       i -= boundaries[j];
       return row_vectors[j]->at(i);
@@ -19,6 +29,6 @@ size_t rowSet::operator[](size_t i) const {
   }
 }
 
-size_t rowSet::size() const {
-  return boundaries.back() + row_vectors.back()->size();
+const size_t& rowSet::size() const {
+  return elements;
 }
--- src/row_set.hpp
+++ src/row_set.hpp
@@ -6,16 +6,22 @@
 
 using namespace std;
 
+// A rowSet allows us to pass around discontinuous sets of row-indices without
+// having to copy subsets of vectors
+
 struct rowSet{
 private:
-  vector<alleleValue> included_alleles;
+  size_t elements;
+  size_t unique_alleles;
+  // the least index, within this struct, of the 1st element of each row_vector
   vector<size_t> boundaries = {0};
   vector<const vector<size_t>* > row_vectors;
+  vector<alleleValue> included_alleles;
 public:
   rowSet();
   rowSet(vector<const vector<size_t>* > row_vectors, vector<alleleValue> allele);
-  size_t operator[](size_t i) const;
-  size_t size() const;
+  const size_t& operator[](size_t i) const;
+  const size_t& size() const;
 };
 
 #endif
